5864d01964f3de37a128409efffae62fea3fa792
User <User@ROMIK>
2024-10-17 18:47:15 +0300

    Добавьте файлы проекта.

---
+++ Webbrowser/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Webbrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Gray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(58, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 15);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(706, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "—";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(736, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "⬜";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(766, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(4, 900);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.panel3.ForeColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(796, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(4, 900);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel4.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.panel4.Location = new System.Drawing.Point(0, 896);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(800, 4);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel5.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(800, 4);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel6.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.panel6.Location = new System.Drawing.Point(0, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(4, 4);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel7.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.panel7.Location = new System.Drawing.Point(796, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(4, 4);
+            this.panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel8.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.panel8.Location = new System.Drawing.Point(0, 896);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(4, 4);
+            this.panel8.TabIndex = 7;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel9.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.panel9.Location = new System.Drawing.Point(796, 896);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(4, 4);
+            this.panel9.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 900);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+    }
+}
+
---
+++ Webbrowser/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Webbrowser
+{
+    public partial class Form1 : Form
+    {
+        int tex = 3;
+        int dex = 4;
+        bool is_move = false;
+        int x = 0, y = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width)/2, textBox1.Location.Y);
+        }
+        private async void move()
+        {
+            await Task.Run(() =>
+            {
+                while (is_move)
+                {
+                    if (WindowState == FormWindowState.Maximized)
+                        if (x != Cursor.Position.X || y != Cursor.Position.Y)
+                        {
+                            x = Size.Width / 2;
+                            y = 0;
+                        }
+                    Location = new Point(Cursor.Position.X - x, Cursor.Position.Y - y);
+                }
+            });
+        }
+        private void resize_window_wh(Size size)
+        {
+            Size = size;
+            panel1.Size = new Size(Size.Width, panel1.Size.Height);
+            button1.Location = new Point(Size.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+        }
+        private void resize_window_wh(int width, int height)
+        {
+            Size size = new Size(width, height);
+            Size = size;
+            panel1.Size = new Size(Size.Width, panel1.Size.Height);
+            button1.Location = new Point(Size.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+        }
+        private void resize_window_mwh(Size size)
+        {
+            Size = size;
+            panel1.Size = new Size(Size.Width, panel1.Size.Height);
+            button1.Location = new Point(Size.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+        }
+        private void resize_window_mwh(int width, int height)
+        {
+            Size size = new Size(width, height);
+            Size = size;
+            panel1.Size = new Size(Size.Width, panel1.Size.Height);
+            button1.Location = new Point(Size.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+        }
+        private void maximize()
+        {
+            WindowState = FormWindowState.Maximized;
+            panel1.Size = new Size(Screen.PrimaryScreen.Bounds.Width, panel1.Size.Height);
+            button1.Location = new Point(Screen.PrimaryScreen.Bounds.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+            panel2.Enabled = false;
+            panel2.Visible = false;
+            panel3.Enabled = false;
+            panel3.Visible = false;
+            panel4.Enabled = false;
+            panel4.Visible = false;
+            panel5.Enabled = false;
+            panel5.Visible = false;
+            panel6.Enabled = false;
+            panel6.Visible = false;
+            panel7.Enabled = false;
+            panel7.Visible = false;
+            panel8.Enabled = false;
+            panel8.Visible = false;
+            panel9.Enabled = false;
+            panel9.Visible = false;
+        }
+        private void normal()
+        {
+            WindowState = FormWindowState.Normal;
+            panel1.Size = new Size(Size.Width, panel1.Size.Height);
+            button1.Location = new Point(Size.Width - button1.Size.Width - 4, button1.Location.Y);
+            button2.Location = new Point(button1.Location.X - button2.Size.Width, button2.Location.Y);
+            button3.Location = new Point(button2.Location.X - button3.Size.Width, button3.Location.Y);
+            textBox1.Size = new Size(panel1.Size.Width / dex * tex, textBox1.Size.Height);
+            textBox1.Location = new System.Drawing.Point((panel1.Size.Width - textBox1.Size.Width) / 2, textBox1.Location.Y);
+            panel2.Enabled = true;
+            panel2.Visible = true;
+            panel3.Enabled = true;
+            panel3.Visible = true;
+            panel4.Enabled = true;
+            panel4.Visible = true;
+            panel5.Enabled = true;
+            panel5.Visible = true;
+            panel6.Visible = true;
+            panel7.Enabled = true;
+            panel7.Visible = true;
+            panel8.Enabled = true;
+            panel8.Visible = true;
+            panel9.Enabled = true;
+            panel9.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                normal();
+            else if (WindowState == FormWindowState.Normal)
+                maximize();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            is_move = false;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            is_move = true;
+            x = e.Location.X; y = e.Location.Y;
+            move();
+        }
+    }
+}
